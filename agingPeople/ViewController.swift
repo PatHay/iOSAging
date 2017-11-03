c61d32c1aed8a1030cec2a5227ecf55c1a94ae72
Patrick Hayes <phayes2828@gmail.com>
2017-11-02 17:26:22 -0700

    completed

--- agingPeople/ViewController.swift
+++ agingPeople/ViewController.swift
@@ -9,9 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var people = ["Adam", "Bill", "Chuck", "David", "Elva", "Fred", "George", "Hector", "Ivan", "Jimbo", "Keith", "Lauren"]
+    
+    var ages: [String] = []
+    
+    func randomAgeGenerator() {
+        var x: Int = 0
+        while x < people.count {
+            ages.append(String(Int(arc4random_uniform(UInt32(99))+1)))
+            x += 1
+        }
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        randomAgeGenerator()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,3 +39,19 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        
+        cell.textLabel?.text = people[indexPath.row]
+        cell.detailTextLabel?.text = ages[indexPath.row] + " years old"
+
+        return cell
+    }
+    
+}
